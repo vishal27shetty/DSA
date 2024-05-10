24130be38e3db456290543558004707985fbbdfc
Vishal Shetty <112746935+vishal27shetty@users.noreply.github.com>
2024-05-10 18:54:07 +0530

    SearchRotatedSorted.cpp

---
+++ SearchRotatedSorted.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int high = nums.size() - 1;
+        int low = 0;
+       
+        while(low<=high)
+        {
+            int mid = (high+low)/2;
+            if(nums[mid]== target)
+                return mid;
+            if(nums[low] <= nums[mid])
+            {
+                if(nums[low]<=target && target <= nums[mid])
+                    high=mid-1;
+                else
+                    low=mid+1;
+            }
+
+            else {
+                if(nums[mid]<=target && target <= nums[high])
+                    low=mid+1;
+                else
+                    high=mid-1;
+            }
+           
+        }
+        return -1;
+    }
+    
+};
